54413708dc8e66b653a0f2f79b31500ba6bb1434
talamortis <talamortis@gmail.com>
2020-07-27 17:25:40 +0100

    mod-Random-Enchants
    Backported Random Enchant module from Azerothcore.

---
+++ modules_scripts/mod-random-enchants/sql/world/item_enchantment_config.sql
@@ -0,0 +1,6 @@
+INSERT INTO `module_config` (`config`, `value`, `comment`) VALUES 
+('RandomEnchants.AnnounceOnLogin', '1', NULL),
+('RandomEnchants.OnLoginMessage', 'Random Enchant Module', NULL),
+('RandomEnchants.OnLoot', '1', NULL),
+('RandomEnchants.OnCreate', '1', NULL),
+('RandomEnchants.OnQuestReward', '1', NULL);
