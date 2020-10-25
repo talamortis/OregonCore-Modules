b130038532e220c54c9c3f13354d355f81adaed8
Micrah <44911744+milestorme@users.noreply.github.com>
2020-10-26 07:24:07 +1100

    update guildhouse.sql (#19)
    fix crash when buying guildhouse when script executes line 63 in mod_guildhouse.cpp

--- modules_scripts/mod_guildHouse/sql/characters/guild_house.sql
+++ modules_scripts/mod_guildHouse/sql/characters/guild_house.sql
@@ -20,6 +20,7 @@ CREATE TABLE IF NOT EXISTS `guild_house` (
   `positionX` float NOT NULL DEFAULT '0',
   `positionY` float NOT NULL DEFAULT '0',
   `positionZ` float NOT NULL DEFAULT '0',
+  `zoneid` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `guild` (`guild`)
 ) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
