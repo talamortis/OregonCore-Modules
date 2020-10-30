659361b2b3723748a298e5838a20b05c3c4f6851
Micrah <44911744+milestorme@users.noreply.github.com>
2020-10-30 14:19:55 +1100

    Solocraft updated and working on OregonCore (#22)

--- modules_scripts/mod-solocraft/sql/world/Solocraft_Conf.sql
+++ modules_scripts/mod-solocraft/sql/world/Solocraft_Conf.sql
@@ -1,6 +1,7 @@
 INSERT INTO `module_config` (`config`, `value`, `comment`) VALUES
 	('Solocraft.Enable', '1', NULL),
 	('Solocraft.Announce', '1', NULL),
-	('Solocraft.Dungeon', '1', NULL),
-	('Solocraft.Heroic', '1', NULL),
-	('Solocraft.Raid', '1', NULL);
+	('Solocraft.CastTimeCheat', '1', NULL),
+	('Solocraft.CooldownCheat', '1', NULL),
+	('Solocraft.Buffs', '1', NULL),
+	('Solocraft.Diff_Power_Multiplier', '5', NULL);
--- modules_scripts/mod-solocraft/src/Solocraft.cpp
+++ modules_scripts/mod-solocraft/src/Solocraft.cpp
@@ -1,4 +1,5 @@
 #include <map>
+
 #include "Config.h"
 #include "ScriptMgr.h"
 #include "Unit.h"
@@ -6,191 +7,181 @@
 #include "Pet.h"
 #include "Map.h"
 #include "Group.h"
-#include "InstanceData.h"
+#include "InstanceSaveMgr.h"
+#include "Log.h"
 #include "Chat.h"
+#include "WorldSession.h"
+#include "SpellMgr.h"
+#include "Spell.h"
 
-// Set the instance difficulty
-int CalculateDifficulty(Map* map, Player* /*player*/) {
-    int difficulty = 1;
-    if (map)
-    {
-        if (map->IsRaid())
-            difficulty = sWorld.GetModuleIntConfig("Solocraft.Raid", 1);
-        else if (map->IsHeroic())
-            difficulty = sWorld.GetModuleIntConfig("Solocraft.Heroic", 1);
-        else if (map->IsDungeon())
-            difficulty = sWorld.GetModuleIntConfig("Solocraft.Dungeon", 1);
-    }
-    return difficulty;
-}
 
-class SolocraftAnnounce : public PlayerScript
-{
-public:
-
-    SolocraftAnnounce() : PlayerScript("SolocraftAnnounce") {}
-
-    void OnLogin(Player* player, bool OnFirstLogin)
-    {
-        // Announce Module
-        if (sWorld.GetModuleBoolConfig("Solocraft.Enable", 1))
-        {
-            if (sWorld.GetModuleBoolConfig("Solocraft.Announce", 1))
-            {
-                ChatHandler(player->GetSession()).SendSysMessage("This server is running the |cff4CFF00SoloCraft |rmodule.");
-            }
-        }
-    }
-};
-
-class solocraft_uint_script : public UnitScript
-{
-public:
-    solocraft_uint_script() : UnitScript("solocraft_uint_script") {}
-
-    void OnHeal(Unit* unit, Unit* reciever, uint32& gain)
-    {
-        if (sWorld.GetModuleBoolConfig("Solocraft.Enable", true))
-        {
-            Player* plr = unit->ToPlayer();
-
-            if (plr)
-            {
-                Group* grp = plr->GetGroup();
-
-                if (grp)
-                    return;
-
-                gain *= CalculateDifficulty(plr->GetMap(), plr);
-            }
-        }
-    }
-
-    void OnDealDamage(Unit* unit, uint32& damage)
-    {
-        if (sWorld.GetModuleBoolConfig("Solocraft.Enable", true))
-        {
-            Player* plr = unit->ToPlayer();
-
-            if (plr)
-            {
-                Group* grp = plr->GetGroup();
-
-                if (grp)
-                    return;
-
-                damage *= CalculateDifficulty(plr->GetMap(), plr);
-            }
-        }
-    }
-};
+namespace {
 
 class solocraft_player_instance_handler : public PlayerScript {
-
 public:
+	solocraft_player_instance_handler() : PlayerScript("solocraft_player_instance_handler") {
+		sLog.outString("scripts.solocraft.player.instance", "[Solocraft] solocraft_player_instance_handler Loaded");
+	}
+	
 
-    solocraft_player_instance_handler() : PlayerScript("solocraft_player_instance_handler") {}
-    
-    void OnMapChanged(Player *player) override {
-        if (sWorld.GetModuleBoolConfig("Solocraft.Enable", true))
-        {
-            Map* map = player->GetMap();
+	
+	void OnLogin(Player *player, bool firstLogin) override {
+		if (sWorld.GetModuleBoolConfig("Solocraft.Announce", true))
+		{
+		ChatHandler(player->GetSession()).SendSysMessage("Solocraft mode activated in dungeons and raids");
+		}
+	}
+	
+
+
+	void OnMapChanged(Player *player) override {
+		if (sWorld.GetModuleBoolConfig("Solocraft.Enable", true)) {
+		Map* map = player->GetMap();
             if (!player->IsAlive())
             {
                 player->ResurrectPlayer(0.7f);
             }
-                
-
-            Group* grp = player->GetGroup();
-
-            if (grp)
-                return;
-
-            int difficulty = CalculateDifficulty(map, player);
-            int numInGroup = GetNumInGroup(player);
-            ApplyBuffs(player, map, difficulty, numInGroup);
-        }
-    }
-
+		int difficulty = CalculateDifficulty(map, player);
+		int numInGroup = GetNumInGroup(player);
+		ApplyBuffs(player, map, difficulty, numInGroup);
+		}
+	}
+	
+	
 private:
+	std::map<ObjectGuid, int> _unitDifficulty;
 
-	std::map<uint32, int> _unitDifficulty;
+	int diff_power_multiplier = sWorld.GetModuleIntConfig("Solocraft.Diff_Power_Multiplier", 100);
 
-    // Get the groups size
-    int GetNumInGroup(Player* player) {
-        int numInGroup = 1;
-        Group *group = player->GetGroup();
-        if (group) {
-            Group::MemberSlotList const& groupMembers = group->GetMemberSlots();
-            numInGroup = groupMembers.size();
-        }
-        return numInGroup;
-    }
+	
+	int CalculateDifficulty(Map *map, Player *player) {
+		int difficulty = 1;
+		if (map) {
+			 if (map->IsHeroic()) {
+				difficulty = 10;
+			} else if (map->IsRaid()) {
+				difficulty = 25;
+			} else if (map->IsDungeon()) {
+				difficulty = 5;
+			}
+		}
+		return difficulty;
+	}
 
-    // Apply the player buffs
-    void ApplyBuffs(Player* player, Map* map, int difficulty, int /*numInGroup*/)
-    {
-        ClearBuffs(player, map);
+	int GetNumInGroup(Player *player) {
+		int numInGroup = 1;
+		Group *group = player->GetGroup();
+		if (group) {
+			Group::MemberSlotList const& groupMembers = group->GetMemberSlots();
+			numInGroup = groupMembers.size();
+		}
+		return numInGroup;
+	}
 
-        if (difficulty > 1)
-        {
-            // InstanceMap *instanceMap = map->ToInstanceMap();
-            // InstanceScript *instanceScript = instanceMap->GetInstanceScript();
+	
+	enum spells
+	{
+		POWER_WORD_FORTITUDE = 25392,
+		PRAYER_OF_SPRITE = 32999,
+		SHADOW_BUFF = 25433,
+		KINGS_BUFF = 25898,
+		ARCANE_BUFF = 27127,
+		MARK_OF_THE_WILD = 26991,
+	};	
+	
+	
+	void ApplyBuffs(Player *player, Map *map, int difficulty, int numInGroup) {
+		ClearBuffs(player, map);
+		if (difficulty > 1) {
+			//InstanceMap *instanceMap = map->ToInstanceMap();
+			//InstanceScript *instanceScript = instanceMap->GetInstanceScript();
 
-            // Announce to player
-            std::ostringstream ss;
-            ss << "|cffFF0000[SoloCraft] |cffFF8000" << player->GetName() << " entered %s  - Difficulty Offset: %d.";
-            ChatHandler(player->GetSession()).PSendSysMessage(ss.str().c_str(), map->GetMapName(), difficulty);
+			ChatHandler(player->GetSession()).PSendSysMessage("Welcome to %s ! You got superpowers!",
+					map->GetMapName(), difficulty, numInGroup);
 
-            // Adjust player stats
-            _unitDifficulty[player->GetGUID()] = difficulty;
-            for (int32 i = STAT_STRENGTH; i < MAX_STATS; ++i)
-            {
-                // Buff the player
-                player->HandleStatModifier(UnitMods(UNIT_MOD_STAT_START + i), TOTAL_PCT, float(difficulty * 100), true);
-            }
+			_unitDifficulty[player->GetGUID()] = difficulty;
+			for (int32 i = STAT_STRENGTH; i < MAX_STATS; ++i) {
+				player->HandleStatModifier(UnitMods(UNIT_MOD_STAT_START + i), TOTAL_VALUE, float((player->getLevel())*difficulty * diff_power_multiplier), true);
+			}
+			
+			//player->ApplySpellPowerBonus((player->getLevel())*difficulty*diff_power_multiplier, true);
+			
+			player->SetFullHealth();
+			
+			if (player->getPowerType() == POWER_MANA) {
+				player->SetPower(POWER_MANA, player->GetMaxPower(POWER_MANA));
+			}
+			
+			if (sWorld.GetModuleBoolConfig("Solocraft.CooldownCheat", true)){
+				player->SetCommandStatusOn(CHEAT_COOLDOWN);
+				ChatHandler(player->GetSession()).PSendSysMessage("Cooldown Cheat is ON. You are not on the global cooldown.");			
+			}			
+			
+			if (sWorld.GetModuleBoolConfig("Solocraft.CastTimeCheat", true)){
+				player->SetCommandStatusOn(CHEAT_CASTTIME);
+				ChatHandler(player->GetSession()).PSendSysMessage("CastTime Cheat is ON. Your spells won't have a casttime.");			
+			}	
+			
+			if (sWorld.GetModuleBoolConfig("Solocraft.Buffs", true)){
+				for(int i = 0; i < 7; i++)
+				player->CastSpell(player, POWER_WORD_FORTITUDE, true);
+				player->CastSpell(player, KINGS_BUFF, true);
+				player->CastSpell(player, MARK_OF_THE_WILD, true);
+				player->CastSpell(player, PRAYER_OF_SPRITE, true);
+				player->CastSpell(player, ARCANE_BUFF, true);
+				player->CastSpell(player, SHADOW_BUFF, true);
+				ChatHandler(player->GetSession()).PSendSysMessage("|cffB400B4You have been buffed, enjoy!");		
+			}
+			
+			
+		}
+	}
 
-            // Set player health & pet
-            player->SetFullHealth();
-            Pet* pet = player->GetPet();
+	void ClearBuffs(Player *player, Map *map) {
+		std::map<ObjectGuid, int>::iterator unitDifficultyIterator = _unitDifficulty.find(player->GetGUID());
+		if (unitDifficultyIterator != _unitDifficulty.end()) {
+			int difficulty = unitDifficultyIterator->second;
+			_unitDifficulty.erase(unitDifficultyIterator);
 
-            if (pet)
-                pet->SetFullHealth();
+			ChatHandler(player->GetSession()).PSendSysMessage("Superpowers turned off.)",
+					map->GetMapName(), difficulty);
 
-
-            if (player->getPowerType() == POWER_MANA)
-            {
-                // Buff the player's health
-                player->SetPower(POWER_MANA, player->GetMaxPower(POWER_MANA));
-            }
-        }
-    }
-
-    void ClearBuffs(Player* player, Map* map)
-    {
-        std::map<uint32, int>::iterator unitDifficultyIterator = _unitDifficulty.find(player->GetGUID());
-        if (unitDifficultyIterator != _unitDifficulty.end())
-        {
-            int difficulty = unitDifficultyIterator->second;
-            _unitDifficulty.erase(unitDifficultyIterator);
-
-            // Inform the player
-            std::ostringstream ss;
-            ss << "|cffFF0000[SoloCraft] |cffFF8000" << player->GetName() << " exited to %s - Reverting Difficulty Offset: %d.";
-            ChatHandler(player->GetSession()).PSendSysMessage(ss.str().c_str(), map->GetMapName(), difficulty);
-
-            // Clear the buffs
-            for (int32 i = STAT_STRENGTH; i < MAX_STATS; ++i)
-            {
-                player->HandleStatModifier(UnitMods(UNIT_MOD_STAT_START + i), TOTAL_PCT, float(difficulty * 100), false);
-            }
-        }
-    }
+			for (int32 i = STAT_STRENGTH; i < MAX_STATS; ++i) {
+				player->HandleStatModifier(UnitMods(UNIT_MOD_STAT_START + i), TOTAL_VALUE, float((player->getLevel())* difficulty * diff_power_multiplier), false);
+			}
+			
+			if (sWorld.GetModuleBoolConfig("Solocraft.CooldownCheat", true)){			
+				player->SetCommandStatusOff(CHEAT_COOLDOWN);
+				ChatHandler(player->GetSession()).PSendSysMessage("Cooldown Cheat is OFF. You are on the global cooldown.");
+			}	
+							
+			if (sWorld.GetModuleBoolConfig("Solocraft.CastTimeCheat", true)){			
+				player->SetCommandStatusOff(CHEAT_CASTTIME);
+				ChatHandler(player->GetSession()).PSendSysMessage("CastTime Cheat is OFF. Your spells will have a casttime.");
+			}	
+			
+			if (sWorld.GetModuleBoolConfig("Solocraft.Buffs", true)){
+				for(int i = 0; i < 7; i++)
+				player->RemoveAurasDueToSpell(POWER_WORD_FORTITUDE);
+				player->RemoveAurasDueToSpell(KINGS_BUFF);
+				player->RemoveAurasDueToSpell(MARK_OF_THE_WILD);
+				player->RemoveAurasDueToSpell(PRAYER_OF_SPRITE);
+				player->RemoveAurasDueToSpell(ARCANE_BUFF);
+				player->RemoveAurasDueToSpell(SHADOW_BUFF);
+				ChatHandler(player->GetSession()).PSendSysMessage("|cffB400B4Your dungeon buffs removed!");		
+			}
+			
+			//player->ApplySpellPowerBonus((player->getLevel())*difficulty*diff_power_multiplier, false);
+			
+				
+		}
+	}
 };
 
-void AddSolocraftScripts()
+}
+
+
+void AddSolocraftScripts() 
 {
-    new solocraft_uint_script();
-    new SolocraftAnnounce();
-    new solocraft_player_instance_handler();
+	new solocraft_player_instance_handler();
 }
